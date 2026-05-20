5c68aaa330293829aa5b2f4749c194bf80fa00ba
Charlton0 <cjuma0909@gmail.com>
2026-05-20 15:21:01 +0300

    first rust commit

---
+++ Hello_world/target/debug/Hello_world.d
@@ -0,0 +1,1 @@
+/mnt/d/Learning_Rust/Hello_world/target/debug/Hello_world: /mnt/d/Learning_Rust/Hello_world/src/main.rs
---
+++ Hello_world/target/debug/deps/Hello_world-5d87f8f7ea216e88.d
@@ -0,0 +1,5 @@
+/mnt/d/Learning_Rust/Hello_world/target/debug/deps/Hello_world-5d87f8f7ea216e88.d: src/main.rs
+
+/mnt/d/Learning_Rust/Hello_world/target/debug/deps/Hello_world-5d87f8f7ea216e88: src/main.rs
+
+src/main.rs:
