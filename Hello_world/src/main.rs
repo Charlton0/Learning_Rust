84230b4747234d7e56c8b1aedf08cf87a02c075d
Charlton0 <cjuma0909@gmail.com>
2026-05-20 15:53:27 +0300

    print! and println! macro

--- Hello_world/src/main.rs
+++ Hello_world/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    println!("I am learning rust");
+    println!("It's awesome"); // println() can be used multiple times and for each it inserts a new line
+    print!("I will insert a new line\n"); //to insert a new line manually with print!() use \n
 }
+
+//fn is a reserved word used to define a function
+//main() is a function
+//println!() is a macro used to display output(printing texts)
+//print!() is similar to println() macro only that it does not insert a new line after, it prints on the same line
+
+ 
